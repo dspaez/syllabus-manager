0945d5e1e87b7483944c321b7caad7f7865237e1
Danny Paez <dpaez1993@gmail.com>
2026-05-28 10:23:28 -0500

    fix: replace JSX.Element with ReactNode to resolve TypeScript build error

--- src/app/(public)/subjects/[id]/WeeksAccordion.tsx
+++ src/app/(public)/subjects/[id]/WeeksAccordion.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import Link from 'next/link';
-import { useMemo, useState } from 'react';
+import { useMemo, useState, type ReactNode } from 'react';
 
 type Material = {
   id: string;
@@ -45,7 +45,7 @@ const TYPE_LABELS: Record<string, string> = {
   ai: 'Presentación',
 };
 
-const GROUP_META: Record<MaterialGroup, { title: string; classes: string; iconClass: string; icon: JSX.Element }> = {
+const GROUP_META: Record<MaterialGroup, { title: string; classes: string; iconClass: string; icon: ReactNode }> = {
   presentations: {
     title: 'Presentaciones',
     classes: 'bg-violet-50 text-violet-700 border-violet-200',
@@ -62,7 +62,7 @@ const GROUP_META: Record<MaterialGroup, { title: string; classes: string; iconCl
     iconClass: 'text-red-700',
     icon: (
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="size-4">
-        <path fillRule="evenodd" d="M4 2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V6.414A2 2 0 0 0 13.414 5L11 2.586A2 2 0 0 0 9.586 2H4Zm4 7a.75.75 0 0 1 .75.75v1.5a.75.75 0 0 1-1.5 0v-1.5A.75.75 0 0 1 8 9Zm-1.5-4.25a.75.75 0 0 0 0 1.5h3a.75.75 0 0 0 0-1.5h-3Z" clipRule="evenodd" />
+        <path fillRule="evenodd" d="M4 2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V6.414A2 2 0 0 0 13.414 5L11 2.586A2 2 0 0 0 9.586 2H4Zm4 7a.75.75 0 0 1 .75.75v1.5a.75.75 0 0 1-1.5 0v-1.5A.75.75 0 0 1 8 9Zm0-5.5a.75.75 0 0 1 .75.75v3a.75.75 0 0 1-1.5 0v-3A.75.75 0 0 1 8 3.5Z" clipRule="evenodd" />
       </svg>
     ),
   },
@@ -187,7 +187,8 @@ function MaterialCard({ material, accentColor }: { material: Material; accentCol
       <TypeBadge material={material} />
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
         className="shrink-0 size-4 text-gray-300 group-hover:text-gray-600 transition-colors">
-        <path fillRule="evenodd" d="M4.25 5.5a.75.75 0 0 0-.75.75v8.5c0 .414.336.75.75.75h8.5a.75.75 0 0 0 .75-.75v-4a.75.75 0 0 1 1.5 0v4A2.25 2.25 0 0 1 12.75 17h-8.5A2.25 2.25 0 0 1 2 14.75v-8.5A2.25 2.25 0 0 1 4.25 4h5a.75.75 0 0 1 0 1.5h-5Zm6.5-3a.75.75 0 0 1 .75-.75h3.5a.75.75 0 0 1 .75.75v3.5a.75.75 0 0 1-1.5 0V3.56l-4.22 4.22a.75.75 0 0 1-1.06-1.06l4.22-4.22h-1.69a.75.75 0 0 1-.75-.75Z" clipRule="evenodd" />
+        <path fillRule="evenodd" d="M4.25 5.5a.75.75 0 0 0-.75.75v8.5c0 .414.336.75.75.75h8.5a.75.75 0 0 0 .75-.75v-4a.75.75 0 0 1 1.5 0v4A2.25 2.25 0 0 1 12.75 17h-8.5A2.25 2.25 0 0 1 2 14.75v-8.5A2.25 2.25 0 0 1 4.25 4h5a.75.75 0 0 1 0 1.5h-5Z" clipRule="evenodd" />
+        <path fillRule="evenodd" d="M6.194 12.753a.75.75 0 0 0 1.06.053L16.5 4.44v2.81a.75.75 0 0 0 1.5 0v-4.5a.75.75 0 0 0-.75-.75h-4.5a.75.75 0 0 0 0 1.5h2.553l-9.056 8.194a.75.75 0 0 0-.053 1.06Z" clipRule="evenodd" />
       </svg>
     </a>
   );
